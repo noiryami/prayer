03dae1deaaf4128be2edacb2ae3b117cda898e47
noiryami <mahmedkamel388@gmail.com>
2025-12-04 23:03:06 +0200

    Fix images

--- src/pages/Prayers/Prayers.jsx
+++ src/pages/Prayers/Prayers.jsx
@@ -3,6 +3,11 @@ import Dauhur from "../../assets/dhuhr.png";
 import Fajr from "../../assets/fajr.png";
 import Isha from "../../assets/isha.png";
 import Maghrib from "../../assets/maghrib.png";
+import Asr2 from "../../assets/asr.jpg";
+import Dauhur2 from "../../assets/dhuhr.jpg";
+import Fajr2 from "../../assets/fajr.jpg";
+import Isha2 from "../../assets/isha.jpg";
+import Maghrib2 from "../../assets/maghrib.jpg";
 import { Link, NavLink } from "react-router-dom";
 import styles from "./styles.module.css"
 
@@ -14,7 +19,7 @@ const Prayers = () => {
       <div className={fullpage}>
         <div className={pic}>
           <Link to={"fajr"} style={{ textDecoration: "none", color: "black" }}>
-            <img src={Fajr} title="الفجر" />
+            <img src={Fajr} src2={Fajr2} title="الفجر" />
             <h2>الفجر</h2>
           </Link>
         </div>
@@ -23,13 +28,13 @@ const Prayers = () => {
             to={"dauhur"}
             style={{ textDecoration: "none", color: "black" }}
           >
-            <img src={Dauhur} title="الظهر" />
+            <img src={Dauhur} src2={Dauhur2} title="الظهر" />
             <h2>الظهر</h2>
           </Link>
         </div>
         <div className={pic}>
           <Link to={"asr"} style={{ textDecoration: "none", color: "black" }}>
-            <img src={Asr} title="العصر" />
+            <img src={Asr} src2={Asr2} title="العصر" />
             <h2>العصر</h2>
           </Link>
         </div>
@@ -38,13 +43,13 @@ const Prayers = () => {
             to={"maghrib"}
             style={{ textDecoration: "none", color: "black" }}
           >
-            <img src={Maghrib} title="المغرب" />
+            <img src={Maghrib} src2={Maghrib2} title="المغرب" />
             <h2>المغرب</h2>
           </Link>
         </div>
         <div className={pic}>
           <Link to={"isha"} style={{ textDecoration: "none", color: "black" }}>
-            <img src={Isha} title="العشاء" />
+            <img src={Isha} src2={Isha2} title="العشاء" />
             <h2>العشاء</h2>
           </Link>
         </div>
